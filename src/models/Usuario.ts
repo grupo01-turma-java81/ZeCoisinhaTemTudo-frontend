aec8d1dbc21522478577cf5edb205ddef90dd17c
ViniCristhian <viniciuscristhian34@gmail.com>
2025-06-06 10:38:26 -0300

    feat: Implementação das models Usuario, UsuarioLogin e Pedido(sem atributos, para que possa ser chamada em Usuario)

---
+++ src/models/Usuario.ts
@@ -0,0 +1,10 @@
+import type Pedido from "./Pedido"
+
+export default interface Usuario {
+    id: number
+    nome: string
+    usuario: string
+    senha: string
+    foto: string
+    pedido: Pedido[] | null
+}
