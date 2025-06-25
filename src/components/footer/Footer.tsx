825adf58debbc2d11e9136d12234d69a2f8424cd
GiulioArantes <giulio.arantes@icloud.com>
2025-06-25 16:49:07 -0300

    fix: Ajustes de informações do footer.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,23 +1,25 @@
 import { useContext, type ReactNode } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 
-
 function Footer() {
+  const ano = new Date().getFullYear();
 
-  const ano = new Date().getFullYear()
+  const { usuario } = useContext(AuthContext);
 
-  const { usuario } = useContext(AuthContext)
-
-  let component: ReactNode
+  let component: ReactNode;
 
   if (usuario.token !== "") {
     component = (
       <footer className="bg-[#1a3052] border-t border-[#16243a] py-3 px-10 flex flex-col sm:flex-row items-start sm:items-center justify-between">
         <div>
-          <a href="https://github.com/grupo01-turma-java81/ZeCoisinhaTemTudo-frontend" target="_blank" rel="noopener noreferrer">
-          <h5 className="text-white text-xl sm:text-2xl font-bold leading-tight">
-            ZéCoisinhaTemTudo
-          </h5>
+          <a
+            href="https://github.com/grupo01-turma-java81/ZeCoisinhaTemTudo-frontend"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h5 className="text-white text-xl sm:text-2xl font-bold leading-tight">
+              ZéCoisinhaTemTudo
+            </h5>
           </a>
           <p className="text-[#b0b8c1] text-xs mt-0.5">
             E-commerce Customer Relationship Management
@@ -27,24 +29,39 @@ function Footer() {
           </p>
         </div>
         <div className="flex gap-6 mt-2 sm:mt-0">
-          <a href="https://github.com/grupo01-turma-java81" target="_blank" rel="noopener noreferrer">
-          <span className="text-[#b0b8c1] text-base font-medium">CodaNervoso</span>
+          <a
+            href="https://github.com/grupo01-turma-java81"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="text-[#b0b8c1] text-base font-medium">
+              Desenvolvedores
+            </span>
           </a>
-          <a href="https://brazil.generation.org/" target="_blank" rel="noopener noreferrer">
-          <span className="text-[#b0b8c1] text-base font-medium">Generation</span>
+          <a
+            href="https://brazil.generation.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="text-[#b0b8c1] text-base font-medium">
+              Generation
+            </span>
           </a>
-          <a href="https://www.metlife.com.br/" target="_blank" rel="noopener noreferrer">
-          <span className="text-[#b0b8c1] text-base font-medium">MetLife</span>
+          <a
+            href="https://www.metlife.com.br/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="text-[#b0b8c1] text-base font-medium">
+              MetLife
+            </span>
           </a>
-          
         </div>
       </footer>
-    )
+    );
   }
 
-  return (
-    component
-  )
+  return component;
 }
 
 export default Footer;
